b7574f0630feee9c6d39dad08c0085606ae25975
youd5 <60477592+youd5@users.noreply.github.com>
2020-01-31 00:46:27 +0530

    unused imports removed

--- src/com/Quote.java
+++ src/com/Quote.java
@@ -1,14 +1,9 @@
 package com;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
