4ba0ee3d0059a331c736b1084a92bf36ba3ff25a
youd5 <60477592+youd5@users.noreply.github.com>
2020-01-31 00:45:19 +0530

    moving to different parser and code moved to a method

--- src/com/Quote.java
+++ src/com/Quote.java
@@ -2,16 +2,20 @@ package com;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 
 /**
 Sample URL from where to pull the data
@@ -29,11 +33,24 @@ public class Quote {
 		System.out.println(urlString);
 		System.out.println("------------------------");
 		try {
-			URL url = new URL(urlString);
-			URLConnection yc = url.openConnection();
-			
-			//Set Request Headers
+			System.out.println(jsonGetRequest(urlString));
+		} catch (Exception e) {
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static String jsonGetRequest(String urlQueryString) {
+		String json = null;
+		try {
+			URL url = new URL(urlQueryString);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setInstanceFollowRedirects(false);
+			connection.setRequestMethod("GET");
 			Map<String, String> headers = new HashMap<>();
+			headers.put("charset","utf-8");
 			headers.put("X-CSRF-Token", "fetch");
 			headers.put("content-type", "application/json");
 			headers.put("Accept", "*/*");
@@ -43,24 +60,20 @@ public class Quote {
 			headers.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0");
 			headers.put("X-Requested-With","XMLHttpRequest");
 			for (String headerKey : headers.keySet()) {
-			    yc.setRequestProperty(headerKey, headers.get(headerKey));
+				connection.setRequestProperty(headerKey, headers.get(headerKey));
 			}
-			
-			
-			
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-			String inputLine;
-			while((inputLine = in.readLine()) != null) 
-				System.out.println(inputLine);
-			JSONObject jsonObject = new JSONObject(inputLine);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			connection.connect();
+			InputStream inStream = connection.getInputStream();
+			json = streamToString(inStream); // input stream to string
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-		
+		return json;
 	}
+
+	private static String streamToString(InputStream inputStream) {
+		String text = new Scanner(inputStream, "UTF-8").useDelimiter("\\Z").next();
+		return text;
+	}
+
 }
